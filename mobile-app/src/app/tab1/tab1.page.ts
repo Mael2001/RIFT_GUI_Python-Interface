2ee03feb76ab0b521a376fa6234d2b748e5ea846
Mario Flores <mariof2001@unitec.edu>
2022-07-10 20:22:35 -0600

    Finished Image and Video, Load, Store and Capture

--- mobile-app/src/app/tab1/tab1.page.ts
+++ mobile-app/src/app/tab1/tab1.page.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
 import { Animation, AnimationController } from '@ionic/angular';
-
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('selectItem', [
+      // ...
+      state('notSelected', style({
+        height: 'auto',
+        opacity: 1,
+        backgroundColor: 'transparent'
+      })),
+      state('selected', style({
+        height: 'auto',
+        opacity: 0.4,
+        backgroundColor: 'grey'
+      })),
+      transition('notSelected => selected', [
+        animate('0.5s')
+      ]),
+      transition('selected => notSelected', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 
 export class Tab1Page {
-
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  selected_images = [];
   constructor(private animationCtrl: AnimationController,
     public photoService: PhotoService) {
   }
@@ -17,23 +45,31 @@ export class Tab1Page {
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
+  uploadImages() {
+    const imageAmount = this.selected_images.length;
+    if(imageAmount>0){
+        alert(`Uploading ${imageAmount} Images`);
+        console.log(this.selected_images);
+    }else{
+      alert(`No images selected, please select some images`);
+    }
+  }
+  getSelected(photo){
+    if (this.selected_images.indexOf(photo) === -1) {
+      return false;
+    }
+    return true;
+  }
   // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
   async ngOnInit() {
-    console.log('Loading Images');
     await this.photoService.loadSaved();
   }
   selectImage(photo) {
-    this.animationCtrl.create()
-      .duration(2000)
-      .iterations(2)
-      .addAnimation(this.animationCtrl.create()
-        .addElement(photo)
-        .duration(5000)
-        .keyframes([
-          { offset: 0, transform: 'scale(1))', opacity: '0.5' },
-          { offset: 0.5, transform: 'scale(0.8)', opacity: '1' },
-          { offset: 1, transform: 'scale(1)', opacity: '0.5' }
-        ]));
+    if (this.selected_images.indexOf(photo) === -1) {
+      this.selected_images.push(photo);
+    }else{
+      this.selected_images = this.selected_images.filter(item => item !== photo);
+    }
     console.log(photo);
   }
 }
